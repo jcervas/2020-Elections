bce29d9a9d5a9464f93b1948a375d50b8b66c1cc
Jonathan Cervas <jcervas@uci.edu>
2020-11-05 14:52:19 -0500

    Updates 2:52pm

--- NYT_json.R
+++ NYT_json.R
@@ -4,8 +4,7 @@ getHouse2020 <- function() {
 	house2020 <- jsonlite::fromJSON("https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/national-map-page/national/house.json")
 	# str(house2020)
 
-	vote.order <- order(house2020$data$races$votes, decreasing=T)
-
+	# vote.order <- order(house2020$data$races$votes, decreasing=T)
 	# house2020.totalvotes <- data.frame(rank=1:435, totalvotes=house2020$data$races$votes[vote.order])
 	# race.name <- paste(house2020$data$races$state_name[vote.order], house2020$data$races$seat[vote.order])
 	# rownames(house2020.totalvotes) <- race.name
@@ -51,7 +50,29 @@ GetPresMargin <- function(STATE = NA) { #Contribution from Nathan Cisneros
 getPresidential2020 <- function() {
 		pres2020 <- jsonlite::fromJSON("https://static01.nyt.com/elections-assets/2020/data/api/2020-11-03/national-map-page/national/president.json")
 		# str(pres2020)
+	rep.votes <- rep(0,51)
+	dem.votes <- rep(0,51)
+	other.votes <- rep(0,51)
+	for (j in 1:length(pres2020$data$races$candidates)) {
+		race.votes.order <- order(pres2020$data$races$candidates[[j]]$votes, decreasing=T)
+		othervotes.tmp <- 0
+			for (i in 1:length(pres2020$data$races$candidates[[j]]$party_id)){
+				party.tmp <- pres2020$data$races$candidates[[j]]$party_id[race.votes.order[i]]
+			
+			if (party.tmp %in% "republican") rep.votes[j] <- pres2020$data$races$candidates[[j]]$votes[race.votes.order[i]]
+			if (party.tmp %in% "democrat") dem.votes[j] <- pres2020$data$races$candidates[[j]]$votes[race.votes.order[i]]
+			if (!party.tmp %in% c("democrat","republican")) {
+						othervotes.tmp <- othervotes.tmp + pres2020$data$races$candidates[[j]]$votes[race.votes.order[i]]
+						other.votes[j] <- othervotes.tmp
+			}
+		}
+	}
+	return(data.frame(st=pres2020$data$races$state_name, ecvotes=pres2020$data$races$electoral_votes, dem=unlist(dem.votes), rep=unlist(rep.votes), other=unlist(other.votes), totalvotes=pres2020$data$races$votes))
+
 }
 
 GetPresMargin("Nevada")
+
+
+write.csv(getPresidential2020(), "/Users/user/Google Drive/GitHub/2020-Elections/pres2020.csv", row.names=F)
 write.csv(getHouse2020(), "/Users/user/Google Drive/GitHub/2020-Elections/house2020.csv", row.names=F)
