6fb5879b17afd7aeefc9c20c31db97c5be832dcf
Jonathan Cervas <jcervas@uci.edu>
2020-11-10 17:25:17 -0500

    Update NYT_json.R

--- NYT_json.R
+++ NYT_json.R
@@ -170,7 +170,7 @@ house <- getHouse2020()
 head(house)
 sum(house$dem)/(sum(house$dem)+sum(house$rep))
 sum(find.winner(two_party(house$dem,house$rep)), na.rm=T)/435
-house
+house[order(abs(house$margin)),]
 
 house.pop.tmp <- jsonlite::fromJSON("https://api.census.gov/data/2019/acs/acs1?get=NAME,B01001_001E&for=congressional%20district:*&key=7865f31139b09e17c5865a59c240bdf07f9f44fd")
 colnames(house.pop.tmp) <- house.pop.tmp[1,]
